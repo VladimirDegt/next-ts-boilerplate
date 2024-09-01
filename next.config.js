ed97f58353dc5409e98b51db515b3b80e434bfa7
Volodymyr <degtyarevvladimirr@gmail.com>
2024-09-01 09:59:55 +0300

    fix: job test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
     pageExtensions: ['tsx', 'ts'],
     sassOptions: {
         prependData: `@import '@/styles/index';`,
